25d691cfde71ede01d982944f602c6dd6bdde347
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-08-31 19:39:00 +0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <h1 class="text-6xl font-bold">Reaction Timer</h1>
-  <button @click="start" v-if="!isPlaying"
-    class="text-white mt-8 bg-teal-600 hover:bg-teal-700 focus:outline-none active:bg-teal-800 font-medium rounded-full text-sm px-8 py-2.5 text-center mb-2 dark:bg-teal-600 dark:hover:bg-teal-600 dark:focus:ring-teal-800">Start</button>
-  <Block v-if="isPlaying" :delay="delay"/>
+  <div class="dark:bg-slate-900 min-h-screen pt-[60px]">
+    <h1 class="text-6xl dark:text-teal-600 font-bold">Reaction Timer</h1>
+    <button @click="start" v-if="!isPlaying"
+      class="text-white mt-8 bg-teal-600 hover:bg-teal-700 focus:outline-none active:bg-teal-800 font-medium rounded-full text-sm px-8 py-2.5 text-center mb-2 dark:bg-teal-600 dark:hover:bg-teal-600 dark:focus:ring-teal-800">Start</button>
+    <Block v-if="isPlaying" :delay="delay" />
+  </div>
 </template>
 
 <script>
@@ -36,9 +38,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-.dis{
-  background-color: lightslategray;
 }
 </style>
