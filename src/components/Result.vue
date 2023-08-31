6f6050ff0cc3a5f3da3b1a3b22efd1b889d79f42
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-08-31 20:16:16 +0500

    Update Result.vue

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,9 +1,26 @@
 <template>
-    <p class= "text-teal-600 mt-8 font-semibold font-lg underline">Your response time is <span class="text-4xl">{{ score }}</span>ms</p>
+    <p class= "text-teal-600 mt-8 font-semibold font-lg underline underline-offset-4">Your response time is <span class="text-4xl">{{ score }}</span>ms</p>
+    <p class= "text-xl text-teal-600 mt-8 font-semibold font-lg ">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-    props: ['score']
+    props: ['score'],
+    data(){
+        return{
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250){
+            this.rank = '🐇 Fast'
+        }
+        else if (this.score < 500){
+            this.rank = '🦊 Average'
+        }
+        else {
+            this.rank = '🐢 Slow Af!'
+        }
+    }
 }
 </script>
