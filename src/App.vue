6248cf3461a2770df960aa5a36345ed5ddf6e971
Saifullah Akhtar <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2023-08-31 19:10:04 +0500

    create a block where to e clicked with bugs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="text-6xl font-bold">Reaction Timer</h1>
+  <button @click="start" v-if="!isPlaying"
+    class="text-white mt-8 bg-purple-700 hover:bg-purple-800 focus:outline-none active:bg-purple-900 font-medium rounded-full text-sm px-8 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Start</button>
+  <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    start() {
+      this.isPlaying = true
+      this.delay = 2000 + Math.random() * 5000
+    }
   }
 }
 </script>
@@ -22,5 +37,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.dis{
+  background-color: lightslategray;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,43 @@
+<template>
+    <p class= "text-green-600 font-semibold font-lg underline" v-if="reactionTime>0">Your response time is {{ reactionTime }}</p>
+    <p v-if="!showBlock"
+        class="mt-12 cursor-pointer font-semibold w-[400px] rounded-xl border-2 border-teal-600 bg-transparent text-black text-center p-[100px] m-auto">
+        click the block asap it appears</p>
+    <div v-if="showBlock" @click="stopTimer"
+        class="mt-12 text-xl cursor-pointer font-semibold w-[400px] rounded-xl bg-teal-600 text-white text-center p-[100px] m-auto">
+        Click Me
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0,
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+        }, this.delay)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            console.log(this.reactionTime)
+        }
+    }
+
+}
+</script>
+
+<style></style>
